09a3a06b766d10efed8bdd31816738e724fa4599
kinetikx <yavuzselim252009@hotmail.com>
2023-03-02 11:44:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        finderBg:"#D1D1D1",
+        primaryBlue:"#2D6DEF",
+        seconderyBlue:"#256BFE"
+      }
+    },
+  },
+  plugins: [],
+}
